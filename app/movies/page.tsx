f772f592a4140d49570bf01dc68b0116c6457fd5
Facundo Herrera <facundhfed@gmail.com>
2023-09-27 15:10:38 +0200

    User and Movies routes created

---
+++ app/movies/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+// type Props = {}
+
+const MoviesPage = () => {
+    return (
+        <main>
+            <h1>Movies</h1>
+        </main>
+    )
+}
+
+export default MoviesPage
---
+++ app/user/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+// type Props = {}
+
+const UserPage = () => {
+    return (
+        <main>
+            <h1>Users</h1>
+        </main>
+    )
+}
+
+export default UserPage
