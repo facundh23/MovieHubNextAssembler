d96b0444f773dbb77c6cd76315b211ecb2f1eb5c
Facundo Herrera <facundhfed@gmail.com>
2023-09-28 14:58:42 +0200

    folder structure completed

---
+++ app/user/[userId]/page.tsx
@@ -0,0 +1,17 @@
+type Props = {
+    params: {
+        userId: string
+    }
+}
+
+
+
+const UserProfile = (props: Props) => {
+    return (
+        <main>
+            <h1>Profile Page</h1>
+        </main>
+    )
+}
+
+export default UserProfile
